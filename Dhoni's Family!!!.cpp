310b6d77d3faab85afea2aa604cb8ee9fdc32630
hari@mk <159900675+harish-mk02@users.noreply.github.com>
2024-02-13 17:33:52 +0530

    Create Dhoni's Family!!!.cpp

---
+++ Dhoni's Family!!!.cpp
@@ -0,0 +1,6 @@
+#include<iostream>
+using namespace std;
+int main(){
+    cout<<"M. S. Dhoni\nFather: Mr. Pan Singh\nMother: Mrs. Devki Devi\nSibings: Narendra Singh and Jayanti Gupta";
+    return 0;
+}
