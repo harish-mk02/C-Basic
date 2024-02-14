3df0d4b09732130281646dcf5a9dd487f2770d53
hari@mk <159900675+harish-mk02@users.noreply.github.com>
2024-02-14 09:16:33 +0530

    Create if statement.cpp

---
+++ if statement.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main(){
+    if(0) {//skip the line if it is false  //not only condition but also boolean - know as expression //should have expression
+    cout<<"0";//can have bodyless if block in our program //if no set braces first line will be taken into if block
+    }
+    cout<<"1";
+        return 0;
+}
